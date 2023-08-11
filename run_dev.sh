6f2297485c33ab0b74984dba79e34d7ca5173073
Slaine W <slainew@gmail.com>
2023-08-11 14:44:37 +0100

    .

--- run_dev.sh
+++ run_dev.sh
@@ -3,3 +3,8 @@ composer install
 ./vendor/bin/sail up -d
 ./vendor/bin/sail -f docker-compose.clickhouse.yml up -d
 ./vendor/bin/sail artisan migrate:fresh
+
+echo '';
+echo '';
+echo '>> This is example for a test running the sms send, look at it to understand sending process <<'
+./vendor/bin/sail artisan test --filter=send_campaign_simple
